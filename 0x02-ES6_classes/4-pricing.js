964fc8248211881d9b260d16d241d136de9a2ad1
gpradinett <gpradinett@gmail.com>
2022-12-01 22:16:58 -0500

    ✅  Implementing a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(mount, currency) {
+    this._amount = mount;
+    this._currency = currency;
+  }
+
+  get mount() {
+    return this._amount;
+  }
+
+  set mount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
