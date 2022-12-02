3fbbe8475318e71c47e831775e086713cb303c5c
gpradinett <gpradinett@gmail.com>
2022-12-01 21:51:51 -0500

    ✅  Constructor attributes:

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,15 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    } else {
-      this._code = code;
-    }
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    } else {
-      this._code = name;
-    }
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
