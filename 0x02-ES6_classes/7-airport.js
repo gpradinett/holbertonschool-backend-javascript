3d4ad735bdc4bc02e5dfb8b08ccebcb1348daa60
gpradinett <gpradinett@gmail.com>
2022-12-01 23:01:08 -0500

    ✅Implement a class named Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
