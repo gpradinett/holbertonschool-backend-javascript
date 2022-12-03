0f3c70e18122e219f74f4b84011ca68c1486646e
gpradinett <gpradinett@gmail.com>
2022-12-02 23:23:42 -0500

    ✅ function accept three arguments

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,17 +5,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpUserPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
-    .then((res) => {
-      const resulList = [];
-
-      resulList.forEach((res) => {
-        if (res.status === 'fulfilled') {
-          resulList.push(res);
-        } else {
-          resulList.push({ status: res.status, value: `${res.reason}` });
-        }
-      });
-      return resulList;
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
+    return result;
+  });
 }
