b2596513ddb35b8b44b5ec4910489967b321864b
gpradinett <gpradinett@gmail.com>
2022-12-06 23:25:17 -0500

    ✅ upload files of task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 const cleanSet = (set, startString) => {
-  if (!startString || startString === undefined || typeof startString === 'undefined' || typeof set !== 'object') return '';
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
   return [...set]
     .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
     .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
