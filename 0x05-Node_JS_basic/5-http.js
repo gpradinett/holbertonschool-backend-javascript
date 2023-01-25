52a29adfadf16aa3acd6cea95146de4afb90d07c
gpradinett <gpradinett@gmail.com>
2023-01-24 23:28:14 -0500

    ✅ upload file

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const args = process.argv.slice(2);
+const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -13,11 +14,12 @@ const app = http.createServer(async (req, res) => {
 
   const { url } = req;
 
-  if (url === '/') res.end('Hello Holberton School!');
-  else if (url === '/students') {
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const students = await countStudents(args.toString());
+      const students = await countStudents(DATABASE);
       res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
@@ -28,7 +30,7 @@ const app = http.createServer(async (req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  // console.log(`Server running at http://${hostname}:${port}/`);
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
