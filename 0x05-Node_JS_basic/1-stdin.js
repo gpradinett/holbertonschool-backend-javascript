f33db0afeec3a921f03dcd00eb4e52513e47d8ee
gpradinett <gpradinett@gmail.com>
2023-01-24 22:46:00 -0500

    ✅ upload file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,3 @@
-//
 process.stdin.setEncoding('utf-8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
@@ -6,7 +5,6 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) process.stdout.write(`Your name is: ${name}\n`)
 });
-//
 if(process.stdout.isTTY) {
   process.stdin.on('end', () => {
     process.stdout.write('This important software is now closing\n');
