7e3b9ae0d1b77b4a63bd9ecb016088276b00b4d7
gpradinett <gpradinett@gmail.com>
2023-01-26 23:57:37 -0500

    ✅ upload file

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const app = express();
-const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`)
+const app = express();
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Welcome to the payment system'));
+app.get('/cart/:id', (req, res) => {
+  if (!isNaN(req.params.id)) res.send(`Payment methods for cart ${req.params.id}`);
+  else res.status(404).end();
 })
 app.get('/available_payments', (req, res) => {
   const obj = {
     payment_methods: {
       credit_cards: true,
-      paypal: false,
-    },
+      paypal: false
+    }
   };
-  res.json(obj);
+  res.send(obj);
 });
+
 app.post('/login', (req, res) => {
-  res.end(`Welcome ${req.body.userName}`);
+  const username = req.body.userName;
+  res.send(`Welcome ${username}`);
 });
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+app.listen(7865, () => console.log('API available on localhost port 7865'));
+
+module.exports = app;
