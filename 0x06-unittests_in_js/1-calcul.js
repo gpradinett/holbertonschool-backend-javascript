8dc0d6f6d24798712ade422dfba63db096229859
gpradinett <gpradinett@gmail.com>
2023-01-26 23:10:12 -0500

    ✅ upload file

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,4 +1,4 @@
-function calculateNumber(type a, b) {
+function calculateNumber(type, a, b) {
   if (type == 'SUM') return Math.round(a) + Math.round(b);
   if (type == 'SUBTRACT') return Math.round(a) - Math.round(b);
   if (type == 'DIVIDE') return Math.round(b) != 0 ? Math.round(a) / Math.round(b) : 'ERROR';
