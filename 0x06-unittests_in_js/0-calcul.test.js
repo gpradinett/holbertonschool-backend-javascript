d5ef20b7183b2c3434a7748272197af872a09c2d
gpradinett <gpradinett@gmail.com>
2023-01-26 22:06:28 -0500

    ✅ upload file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,23 @@
+const calculateNumber = require('./0-calcul');
 const assert = require('assert');
-const calculateNumber = require("./0-calcul.js");
 
-describe('calculateNumber', () => {
-  it('number plus round number', () => {
+describe('calculateNumber', function () {
+  it('...', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('...', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('two round numbers', () => {
+  it('...', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
-  })
-  it('testing with zero', () => {
-    assert.equal(calculateNumber(0, 0), 0);
-    assert.equal(calculateNumber(0, 0.0), 0);
-  })
-  it('number of params', () => {
-    assert(isNaN(calculateNumber()));
-    assert(isNaN(calculateNumber(0,)));
-    assert(isNaN(calculateNumber(0)));
-  })
+  });
+  it('...', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('...', () => {
+    assert.equal(calculateNumber(0.1, 0.2), 0);
+  });
+  it('...', () => {
+    assert.equal(calculateNumber(0.8, 1.7), 3);
+  });
 });
