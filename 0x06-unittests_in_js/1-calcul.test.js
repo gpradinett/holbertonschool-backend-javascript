dec6db89b5915567e729cd58abb11f986d05e475
gpradinett <gpradinett@gmail.com>
2023-01-26 23:13:32 -0500

    ✅ upload file

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,82 +1,17 @@
-const assert = require('assert').strict;
-const calculateNumber = require('./1-calcul');
+const assert = require('assert');
+const calculateNumber = require("./1-calcul.js");
 
-describe('calculateNumber', function () {
-  describe('SUM no Round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUM', 1, 4), 5);
-    });
+describe('calculateNumberWithType', () => {
+  it('', () => {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
   });
-
-  describe('SUM first round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUM', 2.4, 4), 6);
-    });
+  it('', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
-
-  describe('SUM second round ', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUM', 4, 2.4), 6);
-    });
+  it('', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
-
-  describe('SUM both round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
-    });
-  });
-
-  describe('SUBTRACT no round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
-    });
-  });
-
-  describe('SUBTRACT first round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUBTRACT', 2, 4.5), -3);
-    });
-  });
-
-  describe('SUBTRACT second round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUBTRACT', 4.5, 2), 3);
-    });
-  });
-
-  describe('SUBTRACT both round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
-    });
-  });
-
-  describe('DIVIDE no round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('DIVIDE', 8, 4), 2);
-    });
-  });
-
-  describe('DIVIDE first round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('DIVIDE', 9.5, 2), 5);
-    });
-  });
-
-  describe('DIVIDE second round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('DIVIDE', 2, 9.5), 0.2);
-    });
-  });
-
-  describe('DIVIDE both round', function () {
-    it('', function () {
-      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    });
-  });
-
-  describe('DIVIDE Error', function () {
-    it('', function () {
-      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
-    });
+  it('', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'ERROR');
   });
 });
