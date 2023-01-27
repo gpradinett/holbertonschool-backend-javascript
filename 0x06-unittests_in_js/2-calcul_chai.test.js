dcdccb5e58a4294b8889ee89fa8f61e16bf2291f
gpradinett <gpradinett@gmail.com>
2023-01-26 23:18:06 -0500

    ✅ upload file

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require("./1-calcul.js");
+
+describe('calculateNumberWithType', () => {
+  it('', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  it('', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+  it('', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('ERROR');
+  });
+});
