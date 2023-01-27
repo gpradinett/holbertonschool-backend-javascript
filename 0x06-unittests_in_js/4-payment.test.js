a6ee8a3a24286a8b526a3d5fe0dc9b1e7ea6164d
gpradinett <gpradinett@gmail.com>
2023-01-26 23:32:04 -0500

    ✅ upload file

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,18 @@
-const chai = require('chai');
 const sinon = require('sinon');
+var { expect } = require('chai');
+
 const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
-const expect = chai.expect;
 
-
-describe('sendPaymentRequestToApi', function() {
-    it('validate the usage of the Utils function', function() {
-      sinon.spy(Utils, 'calculateNumber');
-      sendPaymentRequestToApi(100, 20);
-      expect(Utils.calculateNumber.calledOnce).to.be.true;
-      expect(Utils.calculateNumber.args[0][0]).to.equal('SUM');
-      expect(Utils.calculateNumber.args[0][1]).to.equal(100);
-      expect(Utils.calculateNumber.args[0][2]).to.equal(20);
-      Utils.calculateNumber.restore();
-    });
+describe('Stubs', function () {
+  it('has the same math', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');
+    stubUtils.returns(10);
+    const spyConsole = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
+    stubUtils.restore();
+    spyConsole.restore();
+  });
 });
