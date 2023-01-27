230208d563ad27572e8733a0636fba09e10836e8
gpradinett <gpradinett@gmail.com>
2023-01-26 23:20:48 -0500

    ✅ upload file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
-const calculateNumber = require("./1-calcul.js");
+const calculateNumber = require("./2-calcul_chai.js");
 
 describe('calculateNumberWithType', () => {
   it('', () => {
